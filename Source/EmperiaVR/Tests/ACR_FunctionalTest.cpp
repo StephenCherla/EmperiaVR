fe46d66cfb30c19f9e19fd0ecc85dd9b0a05ad9f
StephenCherla <stephencherla7@gmail.com>
2023-01-14 23:55:45 +0530

    Unit test toprove that the request gets sent
    Feat - Created Unit test for valid response when valid request is sent

--- Source/EmperiaVR/Tests/ACR_FunctionalTest.cpp
+++ Source/EmperiaVR/Tests/ACR_FunctionalTest.cpp
@@ -8,7 +8,7 @@
 
 
 #if WITH_DEV_AUTOMATION_TESTS
-
+//Unit test for valid request
 IMPLEMENT_SIMPLE_AUTOMATION_TEST(FHttpServiceTest_ValidData, "AcR_Plugin.HttpServiceTest.ValidRequestTest", EAutomationTestFlags::EditorContext | EAutomationTestFlags::ProductFilter)
 bool FHttpServiceTest_ValidData::RunTest(const FString& Parameters)
 {
@@ -20,7 +20,6 @@ bool FHttpServiceTest_ValidData::RunTest(const FString& Parameters)
 	return true;
 }
 
-
 IMPLEMENT_SIMPLE_AUTOMATION_TEST(FHttpServiceTest_InvalidData, "AcR_Plugin.HttpServiceTest.InvalidRequestTest", EAutomationTestFlags::EditorContext | EAutomationTestFlags::ProductFilter)
 bool FHttpServiceTest_InvalidData::RunTest(const FString& Parameters)
 {
@@ -30,6 +29,35 @@ bool FHttpServiceTest_InvalidData::RunTest(const FString& Parameters)
 
 
 	TestEqual(TEXT("Api service should not be called when there is no data in datatable"), false, test);
+	return true;
+}
+
+//Unit test for valid response when valid request is sent
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FSetupHttpService, UHttpService*, HttpService);
+bool FSetupHttpService::Update()
+{
+	HttpService->SetHttpModule();
+	bool test;
+	HttpService->GetRandomIntFromAPI(0, 10, test);
+	return true;
+}
+
+DEFINE_LATENT_AUTOMATION_COMMAND_TWO_PARAMETER(FHttpServiceTest_Response, FAutomationTestBase*, Test, UHttpService*, HttpService);
+bool FHttpServiceTest_Response::Update()
+{
+
+	Test->TestEqual("Api service should send a response when valid request is sent", true, HttpService->IsResponseReceived);
+	return true;
+}
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FHttpServiceTest_RequestAndResponseTest, "AcR_Plugin.HttpServiceTest.RequestAndResponseTest", EAutomationTestFlags::EditorContext | EAutomationTestFlags::ProductFilter)
+bool FHttpServiceTest_RequestAndResponseTest::RunTest(const FString& Parameters)
+{
+
+	UHttpService* HttpService = NewObject<UHttpService>();
+	ADD_LATENT_AUTOMATION_COMMAND(FSetupHttpService(HttpService));
+	ADD_LATENT_AUTOMATION_COMMAND(FWaitLatentCommand(10.0f));
+	ADD_LATENT_AUTOMATION_COMMAND(FHttpServiceTest_Response(this, HttpService));
 	return true;
 }
 
